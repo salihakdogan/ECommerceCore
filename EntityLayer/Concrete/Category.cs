8388645bc4d626a5c1230b406dec707f608ff481
salihakdogan <salihakdogan1@gmail.com>
2022-10-01 09:29:16 +0300

    Add Category.cs and Product.cs

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category //Ctg
+    {
+        public int CategoryID { get; set; }
+        public string ? CtgName { get; set; }
+        public string ? CtgDescription { get; set; }
+        public string ? CtgImage { get; set; }
+        public bool ? CtgStatus { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Product //Prd
+    {
+        public int ProductID { get; set; }
+        public string ? PrdName { get; set; }
+        public string ? PrdDescription { get; set; }
+        public string ? PrdImage1 { get; set; }
+        public string ? PrdImage2 { get; set; }
+        public string ? PrdImage3 { get; set; }
+        public int ? PrdPrice { get; set; }
+        public bool ? PrdStatus { get; set; }
+    }
+}
