63319bb2a2cae5239bb43019d085e1985dc9303f
salihakdogan <salihakdogan1@gmail.com>
2022-10-01 11:31:30 +0300

    Add IGenericService.cs, ICategoryService.cs, IProductService.cs, CategoryManager.cs and ProductManager.cs

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : IGenericService<Category>
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public void TDelete(Category t)
+        {
+            _categoryDal.Delete(t);
+        }
+
+        public Category TGetByID(int id)
+        {
+            return _categoryDal.GetByID(id);
+        }
+
+        public List<Category> TGetList()
+        {
+            return new List<Category>();
+        }
+
+        public void TInsert(Category t)
+        {
+            _categoryDal.Insert(t);
+        }
+
+        public void TUpdate(Category t)
+        {
+            _categoryDal.Update(t);
+        }
+    }
+}
