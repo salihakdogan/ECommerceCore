36de6d7e10fb84e04ef3ddc9c6c64f1690cb3381
salihakdogan <salihakdogan1@gmail.com>
2022-10-01 10:03:04 +0300

    Add migration1.cs, Update Context.cs

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EntityLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.Concrete
@@ -13,5 +14,7 @@ namespace DataAccessLayer.Concrete
         {
             optionsBuilder.UseSqlServer("server=LAPTOP-M3NQRT96;database=ECommerceCore;integrated security=true");
         }
+        public DbSet<Product>Products { get; set; }
+        public DbSet<Category>Categories { get; set; }
     }
 }
---
+++ DataAccessLayer/Migrations/20221001065851_migration1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CtgName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CtgDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CtgImage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CtgStatus = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PrdName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrdDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrdImage1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrdImage2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrdImage3 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrdPrice = table.Column<int>(type: "int", nullable: true),
+                    PrdStatus = table.Column<bool>(type: "bit", nullable: true),
+                    CategoryID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryID",
+                table: "Products",
+                column: "CategoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
