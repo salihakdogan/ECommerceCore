c04f01e96407ab7340fcba82aaea75410a57efb8
salihakdogan <salihakdogan1@gmail.com>
2022-10-02 22:22:18 +0300

    Add HpProductSection1CategoryName.cs and add component of this class. Update some views and components

--- ECommerceCore/ViewComponents/CustomerPage/HomePage/HpProductSection1CategoryName.cs
+++ ECommerceCore/ViewComponents/CustomerPage/HomePage/HpProductSection1CategoryName.cs
@@ -4,12 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceCore.ViewComponents.CustomerPage.HomePage
 {
-    public class HpProductSection1 : ViewComponent
+    public class HpProductSection1CategoryName : ViewComponent
     {
-        ProductManager pmanager = new ProductManager(new EfProductDal());
+        CategoryManager cmanager = new CategoryManager(new EfCategoryDal());
         public IViewComponentResult Invoke()
         {
-            var values = pmanager.TGetList();
+            var values = cmanager.TGetList().OrderBy(x => x.CtgName);
             return View(values);
         }
     }
