2bd208e82c034bdbbc5025e8bb0130b1f1fcef49
salihakdogan <salihakdogan1@gmail.com>
2022-10-06 22:14:28 +0300

    Add Customer.cs, CustomerRole.cs and migration2.cs, Update Context.cs for identity

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -4,17 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.Concrete
 {
-    public class Context : DbContext
+    public class Context : IdentityDbContext<Customer, CustomerRole, int>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("server=LAPTOP-M3NQRT96;database=ECommerceCore;integrated security=true");
         }
-        public DbSet<Product>Products { get; set; }
-        public DbSet<Category>Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
