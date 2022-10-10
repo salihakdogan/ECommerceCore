a7abe99518e4351ccf23292c3a0028a1b3935014
salihakdogan <salihakdogan1@gmail.com>
2022-10-10 15:50:38 +0300

    Add some classes for view or add data to database

---
+++ ECommerceCore/Models/UserLoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceCore.Models
+{
+    public class UserLoginViewModel
+    {
+        [Display(Name ="E-mail")]
+        [Required(ErrorMessage = "E-mail cannot be empty")]
+        public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Password cannot be empty")]
+        public string Password { get; set; }
+    }
+}
---
+++ ECommerceCore/Models/UserRegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceCore.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "Name cannot be empty")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Last name cannot be empty")]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "E-mail cannot be empty")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password cannot be empty")]
+        public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage = "Passwords do not match")]
+        [Required(ErrorMessage = "Verify password cannot be empty")]
+        public string ConfirmPassword { get; set; }
+    }
+}
